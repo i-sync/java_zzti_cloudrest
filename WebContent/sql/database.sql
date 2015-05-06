6aa184e7df99940331f9d6346fd20d4cf78483f2
michael <tianzhenyun@gmail.com>
2015-05-06 22:50:03 +0800

    delete some repeart jar and add database sql

---
+++ WebContent/sql/database.sql
@@ -0,0 +1,34 @@
+#Contacts database script
+
+#1.Create database Contacts
+Create database Contacts;
+
+use Contacts;
+
+#create table Class
+create table Class
+(
+	ID int auto_increment primary key,
+    Name varchar(32),
+    Vocational varchar(128)
+);
+
+#create table Contact
+create table Contact
+(
+	ID int auto_increment primary key,
+    Name varchar(32),
+    Cid int,
+    Phone varchar(16),
+    Email varchar(64),
+    Living varchar(128),
+    Company varchar(128),
+    Remark varchar(1024)
+);
+
+alter table Contact 
+	add column AddDate datetime,
+    add column UpdateDate datetime,
+    add column IP varchar(32);
+
+select * from Contact;
